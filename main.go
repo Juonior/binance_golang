532d55eb422c2c5b586158f89da7dbe1fdb7207d
Juonior <gogame37@gmail.com>
2023-08-15 13:45:12 +0300

    backup local ips

--- main.go
+++ main.go
@@ -8,20 +8,22 @@ import (
 )
 
 func main() {
+	ipAddresses := binance.GetLocalAddresses()
 	binance.CheckToken()
-	proxies, sleepTime, user_min_money, user_max_money, need_spread := binance.GetInfo()
-	fmt.Println(proxies, sleepTime, user_min_money, user_max_money, need_spread)
-	go binance.CheckSell("USDT", []string{"RosBankNew"}, "http://germanshulgapro:RBpNmuqjkT@109.238.200.222:50100")
+	fmt.Println(len(ipAddresses), "Local IPS:", ipAddresses)
+	sleepTime, user_min_money, user_max_money, need_spread := binance.GetInfo()
+	fmt.Println(sleepTime, user_min_money, user_max_money, need_spread)
+	go binance.CheckSell("USDT", []string{"RosBankNew"}, ipAddresses[0])
 	assets := []string{"USDT"}
-	current_proxy_num := 0
+	current_ip_num := 0
 	for {
 		for _, asset := range assets {
-			if current_proxy_num < len(proxies)-1 {
-				current_proxy_num = current_proxy_num + 1
+			if current_ip_num < len(ipAddresses)-1 {
+				current_ip_num = current_ip_num + 1
 			} else {
-				current_proxy_num = 0
+				current_ip_num = 0
 			}
-			go binance.CheckAsset(user_min_money, user_max_money, need_spread, asset, []string{"RosBankNew"}, proxies[current_proxy_num])
+			go binance.CheckAsset(user_min_money, user_max_money, need_spread, asset, []string{"RosBankNew"}, ipAddresses[current_ip_num])
 		}
 		duration := time.Duration(sleepTime) * time.Millisecond
 		time.Sleep(duration)
